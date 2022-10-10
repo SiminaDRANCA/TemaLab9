9d41cdbd4fb80ba79b3a86d1f9ee6ac11f303aad
Simina <simina.dranca@yahoo.com>
2022-10-10 21:01:39 +0300

    Tema Lab 9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,65 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Simina!");
+   /*    System.out.println("Hello Simina!");
+
+
+        int num1 = 5, num2 = 15,sum;
+
+        sum = num1+num2;
+
+        System.out.println("Sum of "+num1+" and "+num2+" is: "+sum);
+
+
+
+        int num3= 105, num4 = 150,AddResult;
+
+        AddResult = num3+num4;
+
+        System.out.println("Sum of "+num3+" and "+num4+" is: "+AddResult);
+
+
+        int a = 57;
+        int b = 17;
+        double r = (double) a / b;
+        System.out.println("Result of "+a+" divided by "+b+" is: "+r);
+
+        int x = -5;
+                int y = 8;
+                        int z = 6;
+        int answer = x + y * z;
+        System.out.println("Result of "+x+" + "+y+" * "+z+" is "+answer);
+
+        int xx = 55;
+        int yy = 9;
+        int answer1 = (xx + yy) % yy;
+        System.out.println("Result of ("+xx+" + "+yy+") % "+yy+" is "+answer1);
+
+        int xxx = 3, yyy = 2;
+
+        int answerD;
+        answerD = num1 + num2 / xxx * yyy - y % xxx;
+        System.out.println("Result of "+num1+" + "+num2+" / "+xxx+" * "+yyy+" - "+y+" % "+xxx+" is "+answerD);*/
+
+    int x = 2;
+    int y = 5;
+
+    int z =( x * y);
+        System.out.println(z);
+
+    float a = multiply(x, y);
+        System.out.println(a);
+
+    float b = divide(x,y);
+        System.out.println(b);}
+
+    public static int multiply(int first, int second) {
+        int result = first * second;
+        return result;
+    }
+
+    public static float divide(float first, float second){
+        float result = first / second;
+        return result;
     }
 }
+
